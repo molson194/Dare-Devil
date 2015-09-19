407242e1c245f8c29e5f5a00e201c0605b2e63d5
Matthew Olson <Olson@matthews-macbook-air-2.local>
2015-09-18 22:59:14 -0400

    Initial Commit

---
+++ Dare Devil/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dare Devil
+//
+//  Created by Matthew Olson on 9/18/15.
+//  Copyright © 2015 Molson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dare Devil/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Dare Devil
+//
+//  Created by Matthew Olson on 9/18/15.
+//  Copyright © 2015 Molson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
